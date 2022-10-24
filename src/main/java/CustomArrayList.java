94e770c1f43bd06c6774674886ed4af2ebb4a2a8
Yaroslav <yaroslav-w@mail.ru>
2022-10-24 04:07:04 +0400

    fix: Добавлена реализация методов. Исправлены ошибки.

--- src/main/java/CustomArrayList.java
+++ src/main/java/CustomArrayList.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import java.util.*;
 
 public class CustomArrayList<E> implements List<E> {
@@ -12,7 +10,7 @@ public class CustomArrayList<E> implements List<E> {
 
   @Override
   public boolean isEmpty() {
-    return array.length > 0;
+    return array.length == 0;
   }
 
   @Override
@@ -41,9 +39,7 @@ public class CustomArrayList<E> implements List<E> {
   @Override
   public Object[] toArray() {
     E[] result = (E[]) new Object[array.length];
-    for (int i = 0; i < array.length; i++) {
-      result[i] = array[i];
-    }
+    System.arraycopy(array, 0, result, 0, array.length);
     return result;
   }
 
@@ -88,18 +84,16 @@ public class CustomArrayList<E> implements List<E> {
 
   @Override
   public boolean addAll(int index, Collection c) {
-    try {
-      E[] temp = array;
-      Object[] secondArray = c.toArray();
-      array = (E[]) new Object[temp.length + secondArray.length];
-      System.arraycopy(temp, 0, array, 0, index);
-      System.arraycopy(secondArray, 0, array, index, secondArray.length);
-      System.arraycopy(temp, index, array, index + secondArray.length, temp.length - index);
-      return true;
-    } catch (IndexOutOfBoundsException ex) {
-      ex.printStackTrace();
+    if (index >= array.length) {
+      throw new IndexOutOfBoundsException();
     }
-    return false;
+    E[] temp = array;
+    Object[] secondArray = c.toArray();
+    array = (E[]) new Object[temp.length + secondArray.length];
+    System.arraycopy(temp, 0, array, 0, index);
+    System.arraycopy(secondArray, 0, array, index, secondArray.length);
+    System.arraycopy(temp, index, array, index + secondArray.length, temp.length - index);
+    return true;
   }
 
   @Override
@@ -186,7 +180,62 @@ public class CustomArrayList<E> implements List<E> {
 
   @Override
   public ListIterator listIterator() {
-    return null;
+    return new ListIterator() {
+      private int currentIndex = 0;
+
+      @Override
+      public boolean hasNext() {
+        return currentIndex < array.length;
+      }
+
+      @Override
+      public Object next() {
+        return array[currentIndex++];
+      }
+
+      @Override
+      public boolean hasPrevious() {
+        return currentIndex > 0;
+      }
+
+      @Override
+      public Object previous() {
+        return array[currentIndex--];
+      }
+
+      @Override
+      public int nextIndex() {
+        if (this.hasNext()) {
+          return currentIndex + 1;
+        } else {
+          return array.length;
+        }
+      }
+
+      @Override
+      public int previousIndex() {
+        if (this.hasPrevious()) {
+          return currentIndex - 1;
+        } else {
+          return -1;
+        }
+      }
+
+      @Override
+      public void remove() {
+
+      }
+
+      @Override
+      public void set(Object o) {
+
+      }
+
+      @Override
+      public void add(Object o) {
+
+      }
+    };
   }
 
   @Override
@@ -195,28 +244,80 @@ public class CustomArrayList<E> implements List<E> {
   }
 
   @Override
-  public List subList(int fromIndex, int toIndex) {
-    return null;
+  public CustomArrayList subList(int fromIndex, int toIndex) {
+    if (toIndex >= array.length) {
+      throw new IndexOutOfBoundsException();
+    }
+    E[] temp = array;
+    this.clear();
+    this.addAll(Arrays.asList(temp).subList(fromIndex, toIndex + 1));
+    return this;
   }
 
   @Override
   public boolean retainAll(Collection c) {
-    return false;
+    Object[] secondArray = c.toArray();
+    var count = array.length;
+    boolean result = false;
+    boolean flag = false;
+    for (int i = 0; i < count; i++) {
+      for (Object o : secondArray) {
+        if (array[i] == o) {
+          result = true;
+        }
+      }
+      if (!result) {
+        this.remove(i);
+        i -= 1;
+        count -= 1;
+        flag = true;
+      } else {
+        result = false;
+      }
+    }
+    return flag;
   }
 
   @Override
   public boolean removeAll(Collection c) {
-    return false;
+    Object[] secondArray = c.toArray();
+    boolean result = false;
+    for (Object o : secondArray) {
+      for (int j = 0; j < array.length; j++) {
+        if (o == array[j]) {
+          this.remove(j);
+          result = true;
+        }
+      }
+    }
+    return result;
   }
 
   @Override
   public boolean containsAll(Collection c) {
-    return false;
+    var secondArray = c.toArray();
+    var result = false;
+    var flag = false;
+    for (Object o : secondArray) {
+      for (E e : array) {
+        if (o.equals(e)) {
+          result = true;
+        }
+      }
+      if (result) {
+        result = false;
+        flag = true;
+      } else {
+        flag = false;
+        break;
+      }
+    }
+    return flag;
   }
 
   @Override
   public Object[] toArray(Object[] a) {
-    return new Object[0];
+    return array;
   }
 
   public CustomArrayList() {
