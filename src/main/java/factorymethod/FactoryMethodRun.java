2892792e283e9bdda275e9a491cff8cece956acd
Yaroslav <yaroslav-w@mail.ru>
2022-10-26 13:45:09 +0400

    feat: Создана новая точка входа в приложение и в ней реализован метод, запускающий соответствующую фабрику согласно переданному параметру.

---
+++ src/main/java/factorymethod/FactoryMethodRun.java
@@ -0,0 +1,40 @@
+package factorymethod;
+
+public class FactoryMethodRun {
+
+  /**
+   * Работает с Фабричным методом.
+   */
+  public static void main(String[] args) {
+
+    // Создание объекта фабрики даты.
+    DateFactory dateFactory = createDateByCountry("russia");
+
+    // Создание объекта даты.
+    Date date = dateFactory.createDate();
+
+    // Вывод даты в консоль
+    date.printDate();
+  }
+
+  /**
+   * Проверяет параметр, содержащий страну и вызывает соответствующую фабрику.
+   *
+   * @param param Название страны.
+   * @return Фабрика даты.
+   */
+  static DateFactory createDateByCountry(String param) {
+
+    // Проверяет какой стране соответствует параметр
+    switch (param) {
+      case ("russia"):
+        return new RussianDateFactory();
+
+      case ("america"):
+        return new AmericanDateFactory();
+    }
+
+    // Если страна не определена, выводит исключение
+    throw new RuntimeException(param + " country don't found");
+  }
+}
